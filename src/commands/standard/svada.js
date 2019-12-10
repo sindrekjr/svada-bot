507dc56d00e966948666edb2ddf4e458ea2c4c58
Sindre S. Kjær <sindre@sindrekjr.no>
2019-12-10 15:43:45 +0100

    Added svada-type param to Svada command

--- src/commands/standard/svada.js
+++ src/commands/standard/svada.js
@@ -4,31 +4,55 @@ const {Command} = require('discord.js-commando');
 const phantom = require('phantom');
 
 module.exports = class Svada extends Command {
-    constructor(bot) {
-        super(bot, {
+    constructor(client) {
+        super(client, {
             name: 'svada',
             group: 'standard',
             memberName: 'svada',
-            description: 'Henter svada fra svadagenerator.no.'
+            description: 'Henter svada fra svadagenerator.no.',
+            args: [
+                {
+                    key: 'content',
+                    type: 'string',
+                    prompt: 'Definer type svada.',
+                    default: 'generell',
+                    /*oneOf: [
+                        'generell',
+                        'arkiv',
+                        'bistand',
+                        'forsikring',
+                        'forsvar',
+                        'helse',
+                        'klima',
+                        'plan',
+                        'bygge',
+                        'mat',
+                        'universitet',
+                        'høgskole',
+                        'skole'
+                    ]*/
+                }
+            ]
         });
     }
 
     run(msg, {content}) {
-        this.get(content, svada => {
+        this.get(this.typeToParam(content), svada => {
             return msg.say(svada);
         });
     }
 
-    get(type = 'generell', callback) {
+    get(type, callback) {
         phantom.create().then(function(instance) {
             return instance.createPage();
         }).then(function(page) {
-            page.open('http://svadagenerator.no/').then(function(status) {
+            page.open('http://svadagenerator.no/?type=' + type).then(function(status) {
                 if (status === 'success') {
                     page.includeJs('http://svadagenerator.no/js/svadagenerator/svadagenerator.js').then(function () {
                         page.evaluate(function () {
                             return document.getElementById('sentence').innerHTML;
                         }).then(function (sentence) {
+                            console.log(type + ": " + sentence);
                             callback(sentence);
                         });
                     });
@@ -37,5 +61,34 @@ module.exports = class Svada extends Command {
                 }
             })
         });
+    }
+
+    typeToParam(input) {
+        switch(input.toLowerCase()) {
+            default: 
+            case 'generell':
+                return 'Generell+svada';
+            case 'arkiv':
+                return 'Arkivsvada'; 
+            case 'bistand':
+                return 'Bistandssvada';
+            case 'forsikring':
+                return 'Forsikringssvada';
+            case 'forsvar': 
+                return 'Forsvarssvada';
+            case 'helse': 
+                return 'Helseadministrativ+svada';
+            case 'klima': 
+                return 'Klimasvada';
+            case 'plan':
+            case 'bygge': 
+                return 'Plan-+og+byggesvada';
+            case 'mat': 
+                return 'Svada+for+Mattilsynet';
+            case 'universitet': 
+            case 'høgskole': 
+            case 'skole':
+                return 'Universitets-+og+høgskolesvada';
+        }
     }
 };
